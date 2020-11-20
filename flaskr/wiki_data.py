dbd97b6c9f344c3f8de8c4bf988b18dd4aad3fde
SebGoliot <50270166+SebGoliot@users.noreply.github.com>
2020-11-20 14:35:54 +0100

    Added wiki_data

---
+++ flaskr/wiki_data.py
@@ -0,0 +1,73 @@
+import requests
+
+class WikiData:
+    
+
+    @classmethod
+    def get_page_desc_from_search(cls, request):
+
+        search_args = {
+            "action": "query",
+            "uselang": "fr",
+            "format": "json",
+            "prop": "extracts",
+            "titles": request,
+            "exintro": 1,
+            "explaintext": 1,
+            "redirects": 1,
+        }
+        data = cls.query_wiki(search_args)
+        return cls.get_desc(data)
+
+    @classmethod
+    def get_page_id_from_position(cls, position):
+
+        search_args = {
+            "action": "query",
+            "uselang": "fr",
+            "format": "json",
+            "list": "geosearch",
+            "gscoord": f"{position['lat']}|{position['lng']}",
+            "gsradius": "100",
+            "gslimit": "1",
+        }
+        data = cls.query_wiki(search_args)
+
+        try:
+            return data["query"]["geosearch"][0]["pageid"]
+        except:
+            return None
+
+    @classmethod
+    def get_page_desc_from_id(cls, page_id):
+
+        search_args = {
+            "action": "query",
+            "uselang": "fr",
+            "format": "json",
+            "prop": "extracts",
+            "pageids": page_id,
+            "explaintext": 1,
+            "redirects": 1,
+        }
+        data = cls.query_wiki(search_args)
+        return cls.get_desc(data)
+
+
+    @staticmethod
+    def get_desc(data):
+
+        data = data["query"]["pages"]
+        extract = data[str(*data)]["extract"]
+
+        return extract.split("\n")[0]
+
+
+    @staticmethod
+    def query_wiki(search_args):
+
+        wiki_api_uri = "https://fr.wikipedia.org/w/api.php"
+        data = requests.get(url=wiki_api_uri, params=search_args)
+
+        return data.json()
+
