4e09941b88d87f676c5e9359b5be50a01538859f
SebGoliot <50270166+SebGoliot@users.noreply.github.com>
2021-01-20 21:49:49 +0100

    Update frontend

--- flaskr/static/js/main.js
+++ flaskr/static/js/main.js
@@ -9,11 +9,11 @@ get_map({lat:42, lng:0}, 2, false);
 
 var user = "You";
 
-
 function ask(){
     var question = $('#message').serializeArray()[0].value;
     $('#message').val('');
     add_msg(user, question);
+    thinking(true);
     $.ajax({
         type: 'POST',
         url: '/ask',
@@ -21,12 +21,15 @@ function ask(){
         contentType: 'text/plain',
         data: question,
         success: function(answer){
+            thinking(false);
             add_msg('GrandPy', answer.response);
             if(answer.position != null){
                 get_map(answer.position, 17);
             }
             if(answer.description != null){
+                thinking(true);
                 setTimeout(function(){
+                    thinking(false);
                     add_msg('GrandPy', answer.description);
                 }, 2500);
             }
@@ -45,8 +48,11 @@ function add_msg(author, content){
 
     var msg = `
         <div class="msg_div `+ author +`">
-            <p class="msg_header">`+ author +` - `+ time +`</p>
-            <p class="msg_content">`+ content +`</p>
+            <div class="content">
+                <p class="msg_header">`+ author +` - `+ time +`</p>
+                <p class="msg_content">`+ content +`</p>
+            </div>
+            <div class="profile"></div>
         </div>
         `;
     $('#messages_wrapper').append(msg);
@@ -57,6 +63,21 @@ function add_msg(author, content){
 function scroll_to_bottom(){
     scrollingElement = document.getElementById('messages_wrapper');
     scrollingElement.scrollTop = scrollingElement.scrollHeight;
+}
+
+
+function thinking(start){
+    if (start == true) {
+        var loader = `
+        <div class="loader_wrapper">
+            <div class="loader"></div>
+        </div>
+        `;
+        $('#messages_wrapper').append(loader);
+    } else {
+        $('.loader_wrapper').remove();
+    }
+    scroll_to_bottom();
 }
 
 
